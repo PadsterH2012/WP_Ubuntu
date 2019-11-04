e785e266e8778483d32493cdfb4e57c259a4d3a6
PadsterH2012 <paddy@bastiondata.com>
2019-11-04 18:54:36 +0000

    Update Test2.sh

--- Test2.sh
+++ Test2.sh
@@ -1,5 +1,56 @@
 #!/bin/bash
-
+## Server Setup
+clear
+echo "Please provide your domain name without the www. (e.g. mydomain.com)"
+read -p "Type your domain name, then press [ENTER] : " MY_DOMAIN
+#read -p "Type your mysql DB ip address, then press [ENTER] : " MY_DOMAIN
+read -p "Type your mysql DB name, then press [ENTER] : " DB_NAME
+read -p "Type your mysql Username, then press [ENTER] : " DB_USERNAME
+read -p "Type your mysql Password, then press [ENTER] : " DB_PASSWORD
+#############################################
 sudo apt-get update && apt get upgrade -y
-sudo apt install apache2
-sudo apt install mariadb-server mariadb-client
+sudo apt install apache2 -y
+##############################################
+sudo apt install mariadb-server mariadb-client -y
+sudo apt install expect -y
+CURRENT_MYSQL_PASSWORD=''
+NEW_MYSQL_PASSWORD=$(openssl rand -base64 29 | tr -d "=+/" | cut -c1-25)
+SECURE_MYSQL=$(expect -c "
+set timeout 3
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"$CURRENT_MYSQL_PASSWORD\r\"
+expect \"root password?\"
+send \"y\r\"
+expect \"New password:\"
+send \"$NEW_MYSQL_PASSWORD\r\"
+expect \"Re-enter new password:\"
+send \"$NEW_MYSQL_PASSWORD\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+echo "${SECURE_MYSQL}"
+echo "CREATE DATABASE $DB_NAME;" | mysql -u root -p$NEW_MYSQL_PASSWORD
+echo "CREATE USER '$DB_USERNAME'@'localhost' IDENTIFIED BY '$userpass';" | mysql -u root -p$NEW_MYSQL_PASSWORD
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USERNAME'@'localhost';" | mysql -u root -p$NEW_MYSQL_PASSWORD
+echo "FLUSH PRIVILEGES;" | mysql -u root -p$NEW_MYSQL_PASSWORD
+sudo apt purge expect -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+#####################################################
+sudo apt install php php-mysql -y
+#####################################################
+cd /tmp && wget https://wordpress.org/latest.tar.gz
+tar -xvf latest.tar.gz
+sudo cp -R wordpress /var/www/html/
+sudo chown www-data:www-data /var/www/html/wordpress/
+sudo chmod -R 755 /var/www/html/wordpress/
+sudo mkdir /var/www/html/wordpress/wp-content/uploads
+sudo chown -R www-data:www-data /var/www/html/wordpress/wp-content/uploads/
