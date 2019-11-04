05fe988006c886d4abcdbf3b3b0cd6186e20151a
PadsterH2012 <paddy@bastiondata.com>
2019-11-04 12:53:09 +0000

    Update FreshInstall.sh

--- FreshInstall.sh
+++ FreshInstall.sh
@@ -16,6 +16,6 @@ sudo apt upgrade -y
 echo "Installing Misc Items"
 sudo apt install open-vm-tools htop apache2 software-properties-common -y
 echo "Installing PHP7.3"
-sudo add-apt-repository ppa:ondrej/apache2
+sudo add-apt-repository ppa:ondrej/php
 sudo apt update -y
 sudo apt install php7.3 php7.3-fpm php7.3-mysql php7.3-mcrypt php-mbstring php-gettext php-curl php7.3-gd -y
