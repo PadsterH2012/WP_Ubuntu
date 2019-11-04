b3191c5a0595284747e8cb98ae15c52f03034643
PadsterH2012 <paddy@bastiondata.com>
2019-11-04 12:19:50 +0000

    Update FreshInstall.sh

--- FreshInstall.sh
+++ FreshInstall.sh
@@ -3,3 +3,13 @@ clear
 echo "Lets Get WordPress Installed !!!"
 echo "Please provide your domain name without the www. (e.g. mydomain.com)"
 read -p "Type your domain name, then press [ENTER] : " MY_DOMAIN
+apt update -y
+apt upgrade -y
+echo "Installing open VM Tools"
+apt install open-vm-tools -y
+echo "Installing hTop"
+apt install htop -y
+echo "Installing nginx"
+apt install nginx -y
+echo "Installing PHP7.3"
+apt install php7.3-fpm php7.3-mysql php7.3-mcrypt php-mbstring php-gettext php-curl php7.3-gd -y
