da9e385b25242771c9fc5af30d76da948a727a33
PadsterH2012 <paddy@bastiondata.com>
2019-11-05 20:22:22 +0000

    Update Test2.sh

--- Test2.sh
+++ Test2.sh
@@ -118,6 +118,7 @@ sudo apt autoremove -y
 sudo apt autoclean -y
 #####################################################
 sudo add-apt-repository ppa:ondrej/php -y
+sudo add-apt-repository ppa:ondrej/apache2 -y
 sudo apt-get update
 sudo apt install php7.3 -y
 sudo apt install imagemagick php7.3-common php7.3-cli php7.3-bcmath php-imagick php7.3-mysql php7.3-bz2 php7.3-curl php7.3-gd php7.3-intl php7.3-json php7.3-mbstring php7.3-readline php7.3-xml php7.3-zip php7.3-fpm -y
