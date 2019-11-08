0ab193315c528f3c9c9b5f0d0b8658fdc1cabf18
PadsterH2012 <paddy@bastiondata.com>
2019-11-08 21:57:48 +0000

    Update Test3.sh

--- Test3.sh
+++ Test3.sh
@@ -12,8 +12,7 @@ read -p "Type your mysql Username, then press [ENTER] : " DB_USERNAME
 #read -p "Type your mysql Password, then press [ENTER] : " DB_PASSWORD
 sudo hostname $MY_DOMAIN
 #############################################
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 yum -y update
 #############################################
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-yum update
-yum install htop
+
