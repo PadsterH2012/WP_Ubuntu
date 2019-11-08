822b4e238d4a9877e8040ec48a3ed620bdc81fb1
PadsterH2012 <paddy@bastiondata.com>
2019-11-08 21:45:08 +0000

    Update Test3.sh

--- Test3.sh
+++ Test3.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 ## Centos Server Setup
+# Pre Req - Install Curl - yum install curl
+# Run as Root
 #
-#
-#
+clear
+echo "Please provide your domain name without the www. (e.g. mydomain.com)"
+read -p "Type your domain name, then press [ENTER] : " MY_DOMAIN
+#read -p "Type your mysql DB ip address, then press [ENTER] : " MY_DOMAIN
+read -p "Type your mysql DB name, then press [ENTER] : " DB_NAME
+read -p "Type your mysql Username, then press [ENTER] : " DB_USERNAME
+#read -p "Type your mysql Password, then press [ENTER] : " DB_PASSWORD
+sudo hostname $MY_DOMAIN
+#############################################
+yum -y update
+#############################################
