4ad0f405ec19200462488e45003933a3ec4edd31
PadsterH2012 <paddy@bastiondata.com>
2019-11-09 10:41:02 +0000

    Update Test3.sh

--- Test3.sh
+++ Test3.sh
@@ -13,10 +13,26 @@ read -p "Type your mysql Username, then press [ENTER] : " DB_USERNAME
 sudo hostname $MY_DOMAIN
 #############################################
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum -y update
 yum -y install yum-utils
 yum-config-manager --disable remi-php54
 yum-config-manager --enable remi-php73
-yum -y install wget nano nginx mariadb mariadb-server php php-common php-mysql php-gd php-xml php-mbstring php-mcrypt
-yum -y update
-#############################################
+yum -y install wget expect nano nginx mariadb mariadb-server php php-mysqlnd php-fpm php-opcache php-gd php-xml php-mbstring
 
+#############################################
+systemctl start nginx
+systemctl enable nginx
+systemctl status nginx
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+systemctl reload firewalld
+chown nginx:nginx /usr/share/nginx/html -R
+#############################################
+systemctl start mariadb
+systemctl enable mariadb
+systemctl status mariadb
+mysql_secure_installation
+#############################################
+systemctl start php-fpm
+systemctl enable php-fpm
+systemctl status php-fpm
