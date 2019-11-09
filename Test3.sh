e560cdfc6c9ac098e65eab19f888ee2ae37cace9
PadsterH2012 <paddy@bastiondata.com>
2019-11-09 12:30:47 +0000

    Update Test3.sh

--- Test3.sh
+++ Test3.sh
@@ -5,7 +5,7 @@
 #
 clear
 echo "Please provide your domain name without the www. (e.g. mydomain.com)"
-#read -p "Type your domain name, then press [ENTER] : " MY_DOMAIN
+read -p "Type your domain name, then press [ENTER] : " MY_DOMAIN
 ##read -p "Type your mysql DB ip address, then press [ENTER] : " MY_DOMAIN
 #read -p "Type your mysql DB name, then press [ENTER] : " DB_NAME
 #read -p "Type your mysql Username, then press [ENTER] : " DB_USERNAME
